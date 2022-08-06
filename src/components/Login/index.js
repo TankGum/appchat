c6aa8fb4a02c115ede739a73a9281bf32f2f6d36
tankgum <aspirineqwe01@gmail.com>
2022-08-07 02:06:14 +0700

    1

---
+++ src/components/Login/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Row, Col, Button, Typography } from 'antd'
+import firebase, { auth } from "../../firebase/config";
+
+const { Title } = Typography
+
+const fbProvider = new firebase.auth.FacebookAuthProvider()
+
+function Login() {
+    const handleFbLogin = () => {
+        auth.signInWithPopup(fbProvider)
+    }
+
+    // auth.onAuthStateChanged((user => ))
+
+    return (
+        <div>
+            <Row justify="center" style={{ height: 800 }}>
+                <Col span={8}>
+                    <Title style={{ textAlign: 'center' }} level={3}>
+                        Fun Chat
+                    </Title>
+                    <Button style={{ width: '100%', marginBottom: 5 }}>
+                        Đăng nhập với Google
+                    </Button>
+                    <Button style={{ width: '100%' }} onClick={handleFbLogin}>
+                        Đăng nhập với Facebook
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore'
+import 'firebase/compat/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQzbA6cPrDfXj3CE2CeIEM0TlTn-o_a78",
+  authDomain: "appchat-5d2d2.firebaseapp.com",
+  projectId: "appchat-5d2d2",
+  storageBucket: "appchat-5d2d2.appspot.com",
+  messagingSenderId: "449955992167",
+  appId: "1:449955992167:web:ca52b89fe9426418c4fac2",
+  measurementId: "G-73KQFB8BNZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
+export { db, auth };
+
+export default firebase;
